d896b362c6e1e2f89030e5cf08018839144b7d3a
sakoda05 <a.sakoda020@fmeets.com>
2024-04-15 22:31:37 +0900

    4-10関数追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(NewsController::class)->prefix('admin')->name('news.')->group(function(){
+    Route::get('news/create', 'add')->name('add');
+});
+
+Route::controller(SelfProfileController::class)->prefix('admin')->name('profile.')->group(function(){
+    Route::get('admin/profile/create', 'add')->name('add');
+    Route::get('admin/profile/edit', 'edit')->name('edit');
+});
